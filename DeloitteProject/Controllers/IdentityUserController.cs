b6c7487cf6f2eafafb4053436086092626c6a13f
SaiVamshiK <saivamshi.k.24@gmail.com>
2021-06-26 16:49:20 +0530

    controller for user identity added

---
+++ DeloitteProject/Controllers/IdentityUserController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using DeloitteProject.Models;
+
+namespace DeloitteProject.Controllers
+{
+    public class IdentityUserController : Controller
+    {
+        private readonly UserManager<IdentityUser> _um;
+        public IdentityUserController(UserManager<IdentityUser> um)
+        {
+            _um = um;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserClass uc)
+        {
+            var user =new IdentityUser{
+                UserName=uc.Email,
+                Email=uc.Email,
+            };
+            var insertRec = await _um.CreateAsync(user,uc.pwd);
+            if(insertRec.Succeeded)
+            {
+                ViewBag.message = "The user with email:" + uc.Email + " is saved successfully";
+
+            }
+            else
+            {
+                foreach(var error in insertRec.Errors)
+                {
+                    ModelState.AddModelError("",error.Description);
+                }
+            }
+            return View();
+        }
+    }
+}
