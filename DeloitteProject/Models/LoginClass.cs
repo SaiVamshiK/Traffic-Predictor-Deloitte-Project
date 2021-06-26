4e22269f445ecca91529b6b9c5d4fcfbea485e4b
SaiVamshiK <saivamshi.k.24@gmail.com>
2021-06-26 17:23:46 +0530

    added the login class for form purpose

---
+++ DeloitteProject/Models/LoginClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DeloitteProject.Models
+{
+    public class LoginClass
+    {
+        [Required(ErrorMessage = "Enter the email")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Enter the Password")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
