2f9ff86c9867a643170c8fe3585d6a1618f491ec
SaiVamshiK <saivamshi.k.24@gmail.com>
2021-06-26 16:32:37 +0530

    added the user class

---
+++ DeloitteProject/Models/UserClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeloitteProject.Models
+{
+    public class UserClass
+    {
+        [Required(ErrorMessage ="Enter the email")]
+        [Display(Name ="Email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Enter the Password")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string pwd { get; set; }
+        [Required(ErrorMessage = "Enter the Password field again")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("pwd")]
+        public string ConfirmPwd { get; set; }
+    }
+}
